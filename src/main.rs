de7d9b124aa066770dad7b6399124b4634898327
marietou334 <chezmarietou@gmail.com>
2026-07-05 18:37:51 +0000

    Licence : Protection du micro-noyau avec la GNU GPLv3 de Richard Stallman

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+/*
+ * LAZE / AXIS Microkernel
+ * Copyright (C) 2026  Sasori
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */ 
+
 #![no_std]
 #![no_main]
 
